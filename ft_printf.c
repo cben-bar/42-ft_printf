fbb6344e265b4127048f5a5646803f9940d1556c
Clementine Ben barek <cben-bar@z4r10p3.42lyon.fr>
2021-12-06 02:41:23 +0100

    -

--- ft_printf.c
+++ ft_printf.c
@@ -5,27 +5,122 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cben-bar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/02 03:38:17 by cben-bar          #+#    #+#             */
-/*   Updated: 2021/12/02 05:10:09 by cben-bar         ###   ########lyon.fr   */
+/*   Created: 2021/12/06 02:38:57 by cben-bar          #+#    #+#             */
+/*   Updated: 2021/12/06 02:39:47 by cben-bar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_printf(const char *str, ...)
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+size_t	ft_strlen(char *str)
 {
-	size_t			i;
-	static size_t	printed;
+	size_t i;
 
 	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	ft_putc(char c, int printed)
+{
+	printed = write(1, &c, 1);
+	return (printed);
+}
+
+int ft_puts(char *str, int printed)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putc(str[i], printed);
+		i++;
+	}
+	return (printed = ft_strlen(str));
+}
+
+int	ft_check_format(char c)
+{
+	char *s;
+	int	i;
+
+	s = "cspdiuxX%";
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (i);
+		i++;
+	}
+	return (i);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	static int	printed;
+	va_list		args;
+	int			i;
+
 	printed = 0;
+	i = 0;
+	va_start(args, str);
 	while (str[i])
 	{
 		while (str[i] != '%')
 		{
-			ft_putchar(str[i], printed);
+			printed += write(1, &str[i], 1);
 			i++;
 		}
 		if (str[i] == '%')
 		{
-			ft_check_flag(str[i + 1]);
+			if (ft_check_format(str[i+1]) > 8);
+				i++;
+			else if (ft_check_format(str[i+1] == 0)
+				ft_putc(
+			else if (ft_check_format(str[i+1] == 1)
+			else if (ft_check_format(str[i+1] == 2)
+			else if (ft_check_format(str[i+1] == 3)
+			else if (ft_check_format(str[i+1] == 4)
+			else if (ft_check_format(str[i+1] == 5)
+			else if (ft_check_format(str[i+1] == 6)
+			else if (ft_check_format(str[i+1] == 7)
+			else if (ft_check_format(str[i+1] == 8)
+				printed += ft_putc('%', printed);
+		}
+		i++;
+	}
+	va_end(args);
+	return (printed);
+}
+
+int main(int ac, char **av)
+{
+	static int printed;	
+	int x;
+	int i;
+	char *str;
+
+	x = 1;
+	printed = 0;
+	str = "+moi";
+	while (x < ac)
+	{ 
+		i = 0;
+		while (av[x][i])
+		{
+			printed += write(1, &av[x][i], 1);
 			i++;
 		}
+		x++;
+	}
+	printed += ft_putc('%', printed);
+	printed += ft_puts(str, printed);
+	printf("\nprinted = %d\n", printed);
+	return (0);
+}
