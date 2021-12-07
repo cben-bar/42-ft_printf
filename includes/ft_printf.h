d0f1d232ed993e85340e70aaeca24b19add46705
Clementine Ben barek <cben-bar@z3r8p3.42lyon.fr>
2021-12-07 04:00:21 +0100

    -

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cben-bar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 22:10:25 by cben-bar          #+#    #+#             */
-/*   Updated: 2021/12/07 02:04:18 by cben-bar         ###   ########lyon.fr   */
+/*   Updated: 2021/12/07 04:00:03 by cben-bar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ int	ft_printf(const char *str, ...);
 int	ft_check_format(va_list args, char c);
 int	ft_printc(char c);
 int	ft_printstr(char *str);
-int	ft_printadd(void *add, size_t printed);
-int	ft_printnbr(int nb);
-int	ft_printunsnbr(unsigned int nb);
+int	ft_printaddr(void *add, size_t printed);
+int	ft_printnbr(int nb, size_t printed);
+int	ft_printunsnbr(unsigned int nb, size_t printed);
 int	ft_printhexnbr(unsigned int nb);
 
 #endif
